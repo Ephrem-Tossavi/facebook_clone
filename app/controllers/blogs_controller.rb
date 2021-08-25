de8a641796d5877c58fd7679dc31108fdec05961
ephrem tossavi <cetossavi@gmail.com>
2021-08-25 23:39:02 +0100

    Confirmation screen

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -21,6 +21,7 @@ class BlogsController < ApplicationController
 
   def confirm
     @blog = current_user.blogs.build(blog_params)
+    #@blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
 
