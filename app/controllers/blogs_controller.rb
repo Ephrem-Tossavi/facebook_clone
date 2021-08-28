38fdece92aa3dcfe949c022fa36c580ccfff8910
ephrem tossavi <cetossavi@gmail.com>
2021-08-28 11:26:48 +0100

    Deletion comment lines in Gemfile and indent correction

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -65,13 +65,13 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:title, :content, :image, :user_id, :image, :image_cache)
-    end
+  # Only allow a list of trusted parameters through.
+  def blog_params
+    params.require(:blog).permit(:title, :content, :image, :user_id, :image, :image_cache)
+  end
 end
